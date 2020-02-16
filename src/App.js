1823b0dbbf99ef5c95f32b07387489a4569c1e82
eshell <eshellster@gmail.com>
2020-02-17 01:11:38 +0900

    할일 추가 setState

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import "./App.css";
 import List from "./List";
 
 function App() {
+  const [newTodo, setNewTodo] = useState();
   const [todos, setTodos] = useState(["js공부", "운동"]);
+
+  const changeInputData = e => {
+    setNewTodo(e.target.value);
+  };
+
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  };
   return (
     <div className="App">
       <h1>todo 애플리케이션</h1>
 
       <form action="">
-        <input type="text" name="" />
-        <button>할일추가</button>
+        <input type="text" name="" onChange={changeInputData} />
+        <button onClick={addTodo}>할일추가</button>
       </form>
       <List todos={todos} />
     </div>
