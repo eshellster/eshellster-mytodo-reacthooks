469ad8be4db911d4fbf65e85c5cc02d29ba20808
eshell <eshellster@gmail.com>
2020-02-17 16:03:00 +0900

    Custom Hooks - useFecth()

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import List from "./List";
 
-function App() {
+const useFecth = (callback, url) => {
+  const [loading, setLoading] = useState(false);
+  const fetchInitialData = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const initialData = await response.json();
+    callback(initialData);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+  return loading;
+};
+
+const App = () => {
   const [newTodo, setNewTodo] = useState();
+
   const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState();
+  const loading = useFecth(setTodos, "http://localhost:8080/todo");
 
   const changeInputData = e => {
     setNewTodo(e.target.value);
@@ -16,21 +32,9 @@ function App() {
     setTodos([...todos, newTodo]);
   };
 
-  const fetchInitialData = async () => {
-    setLoading(true);
-    const response = await fetch("http://localhost:8080/todo");
-    const initialData = await response.json();
-    setTodos(initialData);
-    setLoading(false);
-  };
-
   useEffect(() => {
     console.log("새로운 내용이 랜더링 됐네요", todos);
   }, [todos]);
-
-  useEffect(() => {
-    fetchInitialData();
-  }, []);
 
   return (
     <div className="App">
@@ -43,6 +47,6 @@ function App() {
       <List todos={todos} loading={loading} />
     </div>
   );
-}
+};
 
 export default App;
