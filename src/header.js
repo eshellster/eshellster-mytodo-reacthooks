467717f792d14008b9ef36a337ce6f063715294e
eshell <eshellster@gmail.com>
2020-02-18 18:56:25 +0900

    filter 로 목록 걸러내기

--- src/header.js
+++ src/header.js
@@ -4,7 +4,9 @@ const header = ({ todos }) => {
   return (
     <div>
       <h1>Hello Todo 애플리케이션</h1>
-      <div className="countInfo">해야할일 {todos.length}개가 있습니다.</div>
+      <div className="countInfo">
+        해야할일 {todos.filter(v => v.status === "todo").length}개가 있습니다.
+      </div>
     </div>
   );
 };
